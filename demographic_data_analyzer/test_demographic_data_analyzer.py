43b43693590c83b0dbd2f21990f039520e0e0d21
Vasyl Poremchuk <nleveryone@gmail.com>
2023-01-28 18:14:17 +0200

    Add type annotations to the test functions

--- demographic_data_analyzer/test_demographic_data_analyzer.py
+++ demographic_data_analyzer/test_demographic_data_analyzer.py
@@ -16,7 +16,7 @@ from demographic_data_analyzer.demographic_data_analyzer import (
 ADULT_DF = read_data()
 
 
-def test_read_data():
+def test_read_data() -> None:
     assert isinstance(ADULT_DF, DataFrame)
     assert ADULT_DF.shape[0] > 0
     assert ADULT_DF.shape[1] == 15
@@ -24,25 +24,25 @@ def test_read_data():
     assert "fnlwgt" in ADULT_DF.columns
 
 
-def test_get_number_races():
+def test_get_number_races() -> None:
     race_count = get_number_races(ADULT_DF)
 
     assert race_count == [27816, 3124, 1039, 311, 271]
 
 
-def test_get_average_age_men():
+def test_get_average_age_men() -> None:
     average_age_men = get_average_age_men(ADULT_DF)
 
     assert average_age_men == 39.4
 
 
-def test_get_percentage_bachelors():
+def test_get_percentage_bachelors() -> None:
     percentage_bachelors = get_percentage_bachelors(ADULT_DF)
 
     assert percentage_bachelors == 16.4
 
 
-def test_get_percentage_rich_higher_education():
+def test_get_percentage_rich_higher_education() -> None:
     percentage_rich_higher_education = get_percentage_rich_higher_education(
         ADULT_DF
     )
@@ -50,7 +50,7 @@ def test_get_percentage_rich_higher_education():
     assert percentage_rich_higher_education == 46.5
 
 
-def test_get_percentage_rich_lower_education():
+def test_get_percentage_rich_lower_education() -> None:
     percentage_rich_lower_education = get_percentage_rich_lower_education(
         ADULT_DF
     )
@@ -58,25 +58,25 @@ def test_get_percentage_rich_lower_education():
     assert percentage_rich_lower_education == 17.4
 
 
-def test_get_min_work_hours():
+def test_get_min_work_hours() -> None:
     min_work_hours = get_min_work_hours(ADULT_DF)
 
     assert min_work_hours == 1
 
 
-def test_get_percentage_min_work_hours():
+def test_get_percentage_min_work_hours() -> None:
     percentage_min_work_hours = get_percentage_min_work_hours(ADULT_DF)
 
     assert percentage_min_work_hours == 10.0
 
 
-def test_get_highest_earning_country():
+def test_get_highest_earning_country() -> None:
     highest_earning_country = get_highest_earning_country(ADULT_DF)
 
     assert highest_earning_country == "Iran"
 
 
-def test_get_most_popular_occupation():
+def test_get_most_popular_occupation() -> None:
     most_popular_occupation = get_most_popular_occupation(ADULT_DF)
 
     assert most_popular_occupation == "Prof-specialty"
