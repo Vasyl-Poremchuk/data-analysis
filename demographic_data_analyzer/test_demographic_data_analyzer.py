9af27a0b3d320876e89503a6b0097897f61dd411
Vasyl Poremchuk <nleveryone@gmail.com>
2023-01-28 17:44:59 +0200

    Add test functions to test all cases

---
+++ demographic_data_analyzer/test_demographic_data_analyzer.py
@@ -0,0 +1,82 @@
+from pandas import DataFrame
+
+from demographic_data_analyzer.demographic_data_analyzer import (
+    read_data,
+    get_number_races,
+    get_average_age_men,
+    get_percentage_bachelors,
+    get_percentage_rich_higher_education,
+    get_percentage_rich_lower_education,
+    get_min_work_hours,
+    get_percentage_min_work_hours,
+    get_highest_earning_country,
+    get_most_popular_occupation,
+)
+
+ADULT_DF = read_data()
+
+
+def test_read_data():
+    assert isinstance(ADULT_DF, DataFrame)
+    assert ADULT_DF.shape[0] > 0
+    assert ADULT_DF.shape[1] == 15
+    assert "age" in ADULT_DF.columns
+    assert "fnlwgt" in ADULT_DF.columns
+
+
+def test_get_number_races():
+    race_count = get_number_races(ADULT_DF)
+
+    assert race_count == [27816, 3124, 1039, 311, 271]
+
+
+def test_get_average_age_men():
+    average_age_men = get_average_age_men(ADULT_DF)
+
+    assert average_age_men == 39.4
+
+
+def test_get_percentage_bachelors():
+    percentage_bachelors = get_percentage_bachelors(ADULT_DF)
+
+    assert percentage_bachelors == 16.4
+
+
+def test_get_percentage_rich_higher_education():
+    percentage_rich_higher_education = get_percentage_rich_higher_education(
+        ADULT_DF
+    )
+
+    assert percentage_rich_higher_education == 46.5
+
+
+def test_get_percentage_rich_lower_education():
+    percentage_rich_lower_education = get_percentage_rich_lower_education(
+        ADULT_DF
+    )
+
+    assert percentage_rich_lower_education == 17.4
+
+
+def test_get_min_work_hours():
+    min_work_hours = get_min_work_hours(ADULT_DF)
+
+    assert min_work_hours == 1
+
+
+def test_get_percentage_min_work_hours():
+    percentage_min_work_hours = get_percentage_min_work_hours(ADULT_DF)
+
+    assert percentage_min_work_hours == 10.0
+
+
+def test_get_highest_earning_country():
+    highest_earning_country = get_highest_earning_country(ADULT_DF)
+
+    assert highest_earning_country == "Iran"
+
+
+def test_get_most_popular_occupation():
+    most_popular_occupation = get_most_popular_occupation(ADULT_DF)
+
+    assert most_popular_occupation == "Prof-specialty"
